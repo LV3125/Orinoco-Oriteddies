acaa38b5851f47ff204059d93be2bc5a70aff74e
LV3125 <lauravalez.pro@gmail.com>
2020-10-05 09:46:04 +0200

    Création du dossier js pour les fichiers scripts / Création du script utils.js qui permet de lancer une nouvelle requête vers le serveur / Création du script listeTeddies.js qui permet de communiquer avec le serveur et d'afficher les données des ours en peluche sous forme de liste sur la page html

---
+++ app/js/listeTeddies.js
@@ -0,0 +1,49 @@
+/**
+ * Récupère les informations des ours en peluche depuis l'API
+ * et les affiche grâce à la fonction displayTeddies() qui s'occupe d'afficher la liste des produits
+ */
+
+askRequest('GET','http://localhost:3000/api/teddies', function(bears){
+    displayTeddies(bears);
+});
+
+/**
+ * Affiche la liste des produits (ours en peluche)
+ * */
+const listBears = document.getElementById('fromServer');
+
+function displayTeddies(bears){
+    for(let i in bears) {
+        let productCard = document.createElement('section');
+            productCard.classList.add('row', 'product');
+            
+        let productLeftDiv = document.createElement('div');
+            productLeftDiv.classList.add("col-5", "image-product")
+            productLeftDiv.src = `${bears[i].imageUrl}`;
+            productLeftDiv.style.background = "url(" + productLeftDiv.src + ") no-repeat";
+            productLeftDiv.style.backgroundPosition = "center";
+            productLeftDiv.style.backgroundSize = "cover";
+        
+        let productRightDiv = document.createElement('div');
+            productRightDiv.classList.add("col-7", "title-product");
+
+        let productName = document.createElement('h2');
+            productName.innerText = `${bears[i].name}`;
+
+        let price = `${bears[i].price}`;
+        let priceComa = price.slice(0,2);
+        let productPrice = document.createElement('p');
+            productPrice.innerText = "Prix: " + priceComa + `€`;
+
+        let productDescription = document.createElement('p');
+            productDescription.innerText = `${bears[i].description}`;
+
+        let btnProduct = document.createElement("button");
+            btnProduct.classList.add("btn");
+            btnProduct.innerText = "Plus d'information";
+
+        listBears.append(productCard);
+        productCard.append(productLeftDiv,productRightDiv);
+        productRightDiv.append(productName,productPrice,productDescription, btnProduct);
+    }
+}
---
+++ app/js/utils.js
@@ -0,0 +1,19 @@
+function askRequest(method, url, callback, data){
+    //Création d'une variable à laquelle on assigne une nouvelle requête
+    let request = new XMLHttpRequest(); //initialise un objet
+
+    request.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            callback(JSON.parse(this.responseText, this.status));
+        }
+    }
+
+    //Ouvre et envoi une nouvelle requête au serveur
+    request.open(method, url);
+    request.setRequestHeader("Content-Type", "application/json");
+    if(method == "POST"){
+        request.send(JSON.stringify(data));
+    }else{
+        request.send();
+    }
+}
